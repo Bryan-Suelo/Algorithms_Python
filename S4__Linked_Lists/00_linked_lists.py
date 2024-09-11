bb5545fa64e4007ea70a6ad2a13bfa1648a3fe28
Bryan Campos <c-bryan.camposdionicio@charter.com>
2024-09-11 09:08:11 -0600

    Section 4 - Linked Lists Pop and prepend

--- S4__Linked_Lists/00_linked_lists.py
+++ S4__Linked_Lists/00_linked_lists.py
@@ -16,9 +16,63 @@ class LinkedList:
         while temp is not None:
             print(temp.value)
             temp = temp.next
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
 
-my_linked_list = LinkedList(4)
-print('Head:', my_linked_list.head.value)
-print('Tail:', my_linked_list.tail.value)
-print('Length:', my_linked_list.length)
-print(my_linked_list.print_list())
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        # two or more items in list
+        temp = self.head
+        pre = self.head
+        while temp.next is not None:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp.value
+
+
+# Test if linked list is created correctly
+# my_linked_list = LinkedList(4)
+# print('Head:', my_linked_list.head.value)
+# print('Tail:', my_linked_list.tail.value)
+# print('Length:', my_linked_list.length)
+# print(my_linked_list.print_list())
+
+
+# Test the append function
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.print_list()
+# Test pop
+# print(my_linked_list.pop())     # (2) Items - Returns 2 node
+# print(my_linked_list.pop())     # (1) Items - Returns 1 node
+# print(my_linked_list.pop())     # (0) Items - Returns None
+# Test prepend
+my_linked_list.prepend(0)
+my_linked_list.print_list()
