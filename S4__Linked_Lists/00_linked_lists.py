492582220081ed8493726b193f65e2287be78801
Bryan Campos <c-bryan.camposdionicio@charter.com>
2024-09-16 14:50:59 -0600

    Updating S4 with new functions

--- S4__Linked_Lists/00_linked_lists.py
+++ S4__Linked_Lists/00_linked_lists.py
@@ -55,7 +55,72 @@ class LinkedList:
             self.head = None
             self.tail = None
         return temp.value
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp.value
+    
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            self.pop_first()
+        if index == self.length - 1:
+            self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp.value
 
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        before = None
+        after = temp.next
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
 
 # Test if linked list is created correctly
 # my_linked_list = LinkedList(4)
@@ -66,13 +131,53 @@ class LinkedList:
 
 
 # Test the append function
-my_linked_list = LinkedList(1)
-my_linked_list.append(2)
-my_linked_list.print_list()
+# my_linked_list = LinkedList(11)
+# my_linked_list.append(3)
+# my_linked_list.append(23)
+# my_linked_list.append(7)
+# my_linked_list.print_list()
+# print('\n')
+
 # Test pop
 # print(my_linked_list.pop())     # (2) Items - Returns 2 node
 # print(my_linked_list.pop())     # (1) Items - Returns 1 node
 # print(my_linked_list.pop())     # (0) Items - Returns None
+
 # Test prepend
-my_linked_list.prepend(0)
+# my_linked_list.prepend(0)
+# my_linked_list.print_list()
+
+# Test pop first
+# print(my_linked_list.pop_first())     # (2) Items - Returns 1 node
+# print(my_linked_list.pop_first())     # (1) Items - Returns 2 node
+# print(my_linked_list.pop_first())     # (0) Items - Returns None
+
+# Test get
+# print(my_linked_list.get(2))
+
+# Test set_value
+# my_linked_list = LinkedList(11)
+# my_linked_list.append(3)
+# my_linked_list.append(23)
+# my_linked_list.append(7)
+# my_linked_list.set_value(1, 4)
+# my_linked_list.print_list()  # Output: 11 -> 4 -> 23 -> 7
+
+# Test insert
+# my_linked_list.insert(1, 1)
+# my_linked_list.print_list()  # Output: 11 -> 1 -> 23 ->
+
+# Test remove
+# print(my_linked_list.remove(2), '\n')
+# my_linked_list.print_list()  # Output: 11 -> 23 -> 7
+
+# Test reverse
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
 my_linked_list.print_list()
+print('\n')
+
+my_linked_list.reverse()
+my_linked_list.print_list()  # Output: 4 -> 3 -> 2 ->
